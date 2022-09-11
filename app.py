2496244c5239deb16dd77c473f1b7e4482a31522
ProgRiver <barkas.ssr@mail.ru>
2022-09-11 17:04:13 +0300

    add func live

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/live")
+def live():
+    return render_template("live.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
