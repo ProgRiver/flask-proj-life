80130697d1c611ffbc823c900afa678dae0e4c8b
ProgRiver <barkas.ssr@mail.ru>
2022-09-11 18:14:50 +0300

    game logic

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template
+from game_of_life import GameOfLife
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    GameOfLife(width=25, height=25)
     return render_template("index.html")
 
 @app.route("/live")
 def live():
-    return render_template("live.html")
+    g = GameOfLife()
+    g.form_new_generation()
+    return render_template("live.html", game=g)
 
 
 if __name__ == "__main__":
